b63bb48d31fb1d7ddf92a78c8e3e998e5a80c5fd
GiovaneDiasRocha <66972321+GiovaneDiasRocha@users.noreply.github.com>
2023-09-27 11:12:15 -0300

    Update

--- PokemonReviewApp/Dto/PokemonDto.cs
+++ PokemonReviewApp/Dto/PokemonDto.cs
@@ -1,5 +1,7 @@
-﻿namespace PokemonReviewApp.Data.Dto {
-    public class PokemonDto {
+﻿namespace PokemonReviewApp.Dto
+{
+    public class PokemonDto
+    {
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime BirthDate { get; set; }
